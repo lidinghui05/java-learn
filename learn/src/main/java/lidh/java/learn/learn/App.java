3da28820b3356fc6f2c42d539a56f28230d11011
lidinghui05 <lidinghui05@126.com>
2018-08-07 15:11:16 +0800

    inital

---
+++ learn/src/main/java/lidh/java/learn/learn/App.java
@@ -0,0 +1,13 @@
+package lidh.java.learn.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
